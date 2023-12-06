891603833bb753e0311bd8697424bc047da767f5
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2023-12-06 16:20:06 +0300

    feat: creation Transaction model

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -1,4 +1,66 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Transaction {
+
+  private String id;
+  private String label;
+  private double amount;
+  private LocalDateTime dateTime;
+  private TransactionType type;
+
+  // Enumeration for the transaction type (Debit or Credit)
+  public enum TransactionType {
+    DEBIT, CREDIT
+  }
+
+  public Transaction(String id, String label, double amount, LocalDateTime dateTime,
+                     TransactionType type) {
+    this.id = id;
+    this.label = label;
+    this.amount = amount;
+    this.dateTime = dateTime;
+    this.type = type;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(double amount) {
+    this.amount = amount;
+  }
+
+  public LocalDateTime getDateTime() {
+    return dateTime;
+  }
+
+  public void setDateTime(LocalDateTime dateTime) {
+    this.dateTime = dateTime;
+  }
+
+  public TransactionType getType() {
+    return type;
+  }
+
+  public void setType(TransactionType type) {
+    this.type = type;
+  }
 }
